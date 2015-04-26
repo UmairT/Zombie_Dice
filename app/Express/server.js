b6324134532ab0485f8cab17d06aee6c97b4bf93
Brian Watson <bmwatson888@gmail.com>
2015-04-26 13:49:04 -0700

    updated server.js with socketio includes

--- app/Express/server.js
+++ app/Express/server.js
@@ -3,20 +3,14 @@ var express = require("express"),
 	redis = require("redis"),
 	MongoClient = require('mongodb').MongoClient,
 	app = express(),
-	outcome = "",
-	newUrl,
-	link,
-	genUrl,
-	redirect,
-	testUrl,
-	view,
-	data,
-	found = 0,
+	server = http.createServer(app),
+	socketIO = require('socket.io'),
+	io = socketIO(server),
 	myJson = '{}';
 
 app.use(express.static(__dirname + "/client"));
 
-http.createServer(app).listen(3000);
+server.listen(3000);
 
 app.get("/results.json", function (req, res) {
 	res.json(myJson);
